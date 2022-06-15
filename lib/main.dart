f84af5f904824ee6e5cdc9cac7416c3e49fce8e0
AparnaJoshi555 <aparnajoshi789@gmail.com>
2022-06-15 20:12:19 +0530

    splash screen created

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:arbori/splashscreen.dart';
+import 'package:flutter/material.dart';
+
+void main(List<String> args) => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Digitalized Horticulture App',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: SplashScreen(),
+    );
+  }
+}
---
+++ lib/splashscreen.dart
@@ -0,0 +1,40 @@
+import 'package:arbori/homepage.dart';
+import 'package:arbori/main.dart';
+import 'package:flutter/material.dart';
+//import 'package:flutter_svg/flutter_svg.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    navigatetoscreen();
+  }
+
+  navigatetoscreen() async {
+    await Future.delayed(Duration(milliseconds: 1500));
+    Navigator.pushReplacement(
+        context, MaterialPageRoute(builder: (context) => Home()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+      color: Color.fromRGBO(117, 155, 201, 1),
+      child: Center(
+        child: Text(
+          'ARBORI',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+        //child: SvgPicture.asset('assets/logo.svg'),
+      ),
+    ));
+  }
+}
